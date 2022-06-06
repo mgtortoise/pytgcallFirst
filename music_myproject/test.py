c7e4af3dd2f542e75320ed349a1906f6778b8d44
mgtortoise <75561364+mgtortoise@users.noreply.github.com>
2022-06-06 12:28:42 +0630

    Update test.py

--- music_myproject/test.py
+++ music_myproject/test.py
@@ -22,7 +22,7 @@ bot = pyrogram.Client(
 api_id = '10637638'
 api_hash = '2acc3bec3be2681a2113ef754e3a8c29'
 bot_token = '5088830310:AAEFL6XnXHO8n9dw2kU-sf6I4U8OHxHjQVM'
-session = 'BQBpt34F0skZhBu89RPIrA2zW343oN59cXoGYQID3k9piOnkZpbgMwVMH6Lb9RXgmeZjVXQws39HPeV0Sn6w-_YcgLYhSA6JgcYfGTIYL3vDl_BEB49cWDzrZ4TwopFUSQEn9yeJFqTI5qjesG-A0enbls5m3EYEQlQylLfciq3vb3_IPY3GIaz8heM7Pn-ADkeL9q8-AqwkBO3h9l1KNnY1oz8S6wicd_p5OT9QEnjTAv1ID8oIUk_jxo4REYrdSfix4dV7s66Ud2oyGbQH3ESZgK5Cw8EJeMoLmtqybcRCRIgBfFEkx7CL9VmloH7GjVS35AQRuLEpN6N7qDR-rCskAAAAATPwVagA'
+session = 'BQBXlcL3GJkPvd5ov2BCfiXIj1XfsnvsyV3KBaFf5rMIX2PV4qkbFnQoDRDEdXuCCXphFKuuEp4uabPdrTyffYgQvMNxKehstV5U3Zoo-xmIl2KHU5Qa2t-NBUgXdtGK0RAMVgEcHpadEAH6-sI2_9QcoXidBeFXHJ4mgSJ0odhJYwQ3fl9ivcw-Chnya3C1tnQtXCKcbZDeHNArbTFMJugfrHDE9G9Fw5DRLr2Pl5jfVad3hznH_ClEoOsdtjM9rO2KqcsRwf9davVeARnodEBVBwW_olSwMBEhIsL6f1EBIji5oVj2hk1c_GmjH0VNENgmtNmP_nZspH3AumG4ZdwXAAAAAS9RY2YB'
 
 client = Client(session, api_id, api_hash)
 
@@ -110,7 +110,7 @@ async def leave(bot,msg):
 @bot.on_message(filters.command('add'))
 async def song_add(bot,msg):
     text = msg.text.split()[1]
-    pytube_me.vid_download(url)
+    pytube_me.vid_download(text)
     await bot.send_message(msg.chat.id,'You added')
 
     #pytube_me.download(text)
